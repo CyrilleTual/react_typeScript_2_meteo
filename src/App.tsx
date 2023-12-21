f049f6fffd6621bc6841c50c35d050165503f11b
cyrilletual <cyrille.tual@gmail.com>
2023-12-21 19:16:24 +0100

    modif svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
- 
+import React from "react";
+import style from "./App.module.css";
+import loader from "./assets/loader.svg";
+//import img1 from"./assets/icons2/cloudy-day-1.svg"
 
 function App() {
   return (
-    <div className="App">
-       <h1>hello</h1>
-    </div>
+    <main>
+      <div className={style.loader_container}>
+        <img src={loader} alt="loading" />
+      </div>
+      <p className={style.city_name}>Paris</p>
+      <p className={style.country_name}>France</p>
+      <p className={style.temperature}>17°</p>
+      <div className={style.info_icon_container}>
+        <img src={require("./assets/icons2/cloudy-day-1.svg").default} alt="" />
+      </div>
+    </main>
   );
 }
 
--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,1 +1,16 @@
 declare module "*.css";
+// declare module "*.jpg" {
+//   const path: string;
+//   export default path;
+// }
+// declare module "*.svg" {
+//   const path: string;
+//   export default path;
+// }
+
+declare module "*.svg" {
+  import React = require("react");
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
